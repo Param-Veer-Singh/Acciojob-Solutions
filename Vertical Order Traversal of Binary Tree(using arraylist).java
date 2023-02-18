b05075223cec424cf7de4a786e8e2aeef04129b7
Param-Veer-Singh <122340319+Param-Veer-Singh@users.noreply.github.com>
2023-02-18 17:48:51 +0530

    Create Vertical Order Traversal of Binary Tree(using arraylist).java

---
+++ Vertical Order Traversal of Binary Tree(using arraylist).java
@@ -0,0 +1,125 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class TreeNode{
+    int data;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class Solution {
+	int maxRightNode = 0;
+	int minLeftNode = 0;
+	void getWidth(TreeNode root, int pos){
+		if(root == null){
+			return;
+		}
+		getWidth(root.left , pos-1);
+		getWidth(root.right , pos+1);
+
+		maxRightNode = Math.max(maxRightNode,pos);
+		minLeftNode = Math.min(minLeftNode,pos);
+		
+	}
+	class Pair{
+		TreeNode node ;
+		int pos = 0;
+		Pair(TreeNode node, int pos){
+			this.node = node;
+			this.pos=  pos;
+		}
+	}
+    List<List<Integer>> VerticalTraversal(TreeNode root) {
+        // get width of the tree i.e distance between min left and max righ nodes
+		getWidth(root, 0);
+		int width = maxRightNode - minLeftNode;
+
+		int level = width + 1;
+		List<List<Integer>> vv = new ArrayList<>();
+		for(int i=0; i<level; i++){
+			vv.add(new ArrayList<>());
+		}
+
+		Queue<Pair> que = new ArrayDeque<>();
+		que.add(new Pair(root, -minLeftNode));
+		while(que.size()>0){
+			int size = que.size();
+			while(size-->0){
+				Pair curr = que.remove();
+
+				vv.get(curr.pos).add(curr.node.data);
+
+				if(curr.node.left!=null){
+					que.add(new Pair(curr.node.left, curr.pos-1));
+				}
+				if(curr.node.right!=null){
+					que.add(new Pair(curr.node.right, curr.pos+1));
+				}
+			}
+		}
+		for(int i=0; i<level; i++){
+			Collections.sort(vv.get(i));
+		}
+		return vv;
+    }
+}
+
+class Main {
+
+    static TreeNode buildTree(String str) {
+        if(str.length()==0 || str.charAt(0)=='N')
+            return null;
+
+        String ip[] = str.split(" ");
+        TreeNode root = new TreeNode(Integer.parseInt(ip[0]));
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            TreeNode currNode = queue.peek();
+            queue.remove();
+            String currVal = ip[i];
+            if(!currVal.equals("N")) {
+                currNode.left = new TreeNode(Integer.parseInt(currVal));
+                queue.add(currNode.left);
+            }
+            i++;
+            if(i >= ip.length)
+                break;
+            currVal = ip[i];
+            if(!currVal.equals("N")) {
+                currNode.right = new TreeNode(Integer.parseInt(currVal));
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        return root;
+    }
+
+    public static void main (String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(br.readLine());
+        while(t > 0){
+            String s = br.readLine();
+            TreeNode root = buildTree(s);
+            Solution ob = new Solution();
+            List<List<Integer>> ans = ob.VerticalTraversal(root);
+            for(int i = 0; i < ans.size(); i++) {
+                for(int a : ans.get(i)) {
+                    System.out.print(a + " ");
+                }
+                System.out.println();
+            }
+           // System.out.println();
+            t--;
+        }
+    }
+}
